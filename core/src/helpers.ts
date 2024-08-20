fb6520b4c749649e83a14168960e4f6ce15e125f
Armen Babakanian <armen.babakanian@gmail.com>
2024-08-20 07:22:11 -0700

    added readme

--- core/src/helpers.ts
+++ core/src/helpers.ts
@@ -1,73 +1,11 @@
 import { v4 as uuidv4 } from "uuid"
 
-import {
-  // BooleanResponse,
-  // BooleanAttribute,
-  // MultiSelectAttribute,
-  Prompt,
-  // ResponseAttributeBase,
-  ExchangeInput,
-  Exchange,
-  ResponseAttributes,
-} from "./types"
+import { Prompt, ExchangeInput, Exchange, ResponseAttributes } from "./types"
 
 export function createPrompt(prompt: Prompt): Prompt {
   return prompt
 }
 
-// export function createMultiSelectPrompt<T>(
-//   options: Array<MultiSelectAttribute>
-// ): MultiSelectAttribute {
-//   const newOptions = options.map((item, index) => {
-//     return {
-//       id: `${index}`,
-//       ...item,
-//     }
-//   })
-
-//   return {
-//     options: newOptions,
-//   }
-// }
-
-// export function isMultiSelectPrompt(
-//   input: ResponseAttributeBase
-// ): input is MultiSelectAttribute {
-//   return (input as MultiSelectAttribute).options !== undefined
-// }
-
-// export function createMultiSelectResponse(
-//   options: Array<MultiSelectAttributeInput>
-// ): MultiSelectAttribute {
-//   const newOptions = options.map((item, index) => {
-//     return {
-//       id: `${index}`,
-//       ...item,
-//     }
-//   })
-
-//   return {
-//     options: newOptions,
-//   }
-// }
-
-// export function isBooleanPrompt(
-//   input: ResponseAttributeBase
-// ): input is BooleanAttribute {
-//   return (input as BooleanAttribute).shouldHaveUndecided !== undefined
-// }
-
-// export function createBooleanResponse(
-//   value: boolean,
-//   undecided?: boolean
-// ): BooleanResponse {
-//   return {
-//     value,
-//     undecided,
-//   }
-// }
-
-// -------
 export function createExchange<T extends ResponseAttributes>(
   input: ExchangeInput<T>
 ): Exchange<T> {
---
+++ readme.md
@@ -0,0 +1,20 @@
+# PQSS | Programmable Query Survey System
+
+This is an open-source typescript system comprised of smaller libraries enabling you to create complex survey systems using code.
+The goal is to have a fully compliant Typescript API, that you can create general surveys such for creating tests for students, university research, ux/ui surveys and so much more.
+
+> The library is in the very early phases so do not use in production.
+
+## Current Feature list:
+
+1. Using API calls, create an run a survey
+2. add conditional logic
+3. typescript autocomplete checking
+
+## Work In progress:
+
+1. React based UI to collect responses
+2. Refactor code, separate out responses to a different collection
+3. Vue based UI to collect responses
+4. Angular based UI to collect responses
+5. svelte based UI to collect response
